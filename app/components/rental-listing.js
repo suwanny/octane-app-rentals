9f8fd96e491d6e9586405d3a4ec5dfe35f2ac829
Soo Hwan Park <sopark@linkedin.com>
2019-03-26 05:16:48 -0400

    Fixing tracked

--- app/components/rental-listing.js
+++ app/components/rental-listing.js
@@ -1,7 +1,11 @@
-import Component, { tracked } from '@glimmer/component';
+import Component from '@glimmer/component';
+
+// Gotcha from Discord
+// Mike N: in alpha.4, I'm seeing issues where tracked works fine when imported from @glimmer/tracking,
+// but fails when imported from @glimmer/component
+import { tracked } from '@glimmer/tracking';
 
 export default class RentalListingComponent extends Component {
-  // @tracked
   @tracked isWide = false;
 
   toggleImageSize() {
